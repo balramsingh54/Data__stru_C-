5c51f9a60abf987028fcbb74323466a7b36fdc98
Balram <singhbalram054@gmail.com>
2019-07-18 21:07:57 +0530

    binary search tree

--- tree/tree.cpp
+++ tree/tree.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <queue>
+#include <cmath>
 
 using namespace std;
 
@@ -52,32 +54,237 @@ void displayPostOrder(node*root){
 
 	cout<<root->data<<" ";
 }
-void displayInOrder(node*root){
+
+void displayInOrder(node* root){
 	if(root==NULL){
 		return;
 	}
 
 	displayInOrder(root->left);
+
 	cout<<root->data<<" ";
+
 	displayInOrder(root->right);
+}
+
+void displayLevelOrder(node* root){
+	queue<node*> q;
+
+	q.push(root);
+
+	while(!q.empty()){
+		node* temp = q.front();
+
+		q.pop();
+
+		cout<<temp->data<<" ";
+
+		if(temp->left!=NULL){
+			q.push(temp->left);
+		}
+
+		if(temp->right!=NULL){
+			q.push(temp->right);
+		}
 
 	}
+	cout<<endl;
+}
+
+void displayLevelOrderNewLine(node*root){
+	queue<node*> q;
+	q.push(root);
+	q.push(NULL);
+
+	while(!q.empty()){
+		node* temp = q.front();
+		q.pop();
+
+		if(temp==NULL){
+			cout<<endl;
+			if(!q.empty()){
+				q.push(NULL);
+			}
+			continue;
+		}
+
+		cout<<temp->data<<" ";
+
+		if(temp->left!=NULL){
+			q.push(temp->left);
+		}
+
+		if(temp->right!=NULL){
+			q.push(temp->right);
+		}
+
+	}
+	cout<<endl;
+}
+
+int numberOfNodes(node* root){
+	if(root==NULL){
+		return 0;
+	}
+
+	int count;
+
+	int left_count = numberOfNodes(root->left);
+	int right_count = numberOfNodes(root->right);
+
+	count = left_count + right_count + 1;
+	return count;
+}
+
+int sumOfNodes(node*root){
+	if(root==NULL){
+		return 0;
+	}
+
+	int sum;
+	int left_sum = sumOfNodes(root->left);
+	int right_sum = sumOfNodes(root->right);
+
+	sum = left_sum + right_sum + root->data;
+	return sum;
+}
+
+int height(node*root){
+	if(root==NULL){
+		return -1;
+	}
+
+	int left_height = height(root->left);
+	int right_height = height(root->right);
+
+	return max(left_height,right_height) + 1;
+}
+
+int diameter(node*root){
+	if(root==NULL){
+		return 0;
+	}
+
+	int left_dia = diameter(root->left);
+	int right_dia = diameter(root->right);
+
+	int leftH = height(root->left);
+	int rightH = height(root->right);
+	int total_dia = leftH + rightH + 2;
+
+	return max(total_dia,max(left_dia,right_dia));
+}
+
+bool isBalanced(node* root){
+	if(root==NULL){
+		return true;
+	}
+
+	bool leftBalanced = isBalanced(root->left);
+	bool rightBalanced = isBalanced(root->right);
+
+	if(!leftBalanced or !rightBalanced){
+		return false;
+	}
+
+	int leftH = height(root->left);
+	int rightH = height(root->right);
+
+	int gap = abs(leftH - rightH);
+
+	if(gap>1){
+		return false;
+	}else{
+		return true;
+	}
+
+}
+
+class L2{
+public:
+	int height;
+	int diameter;
+};
+
+L2 diameterOptimized(node*root){
+	L2 val;
+
+	if(root==NULL){
+		val.height = -1;
+		val.diameter = 0;
+		return val;
+	}
+
+	L2 left = diameterOptimized(root->left);
+	L2 right = diameterOptimized(root->right);
+
+	val.height = max(left.height,right.height) + 1;
+	val.diameter = max(left.height + right.height + 2,max(left.diameter,right.diameter));
+	return val;
+}
+
+node* buildTreeFromArray(int arr[], int s, int e)
+{
+	if (root==NULL)
+	{
+		return;
+	}
+	int m= (s+e)/2;
+	node* root= new node[m];
+	root->left=buildTreeFromArray(arr,s,m-1);
+	root->right=buildTreeFromArray(arr,m+1,e);
+	return root;
+
+}
 
 
 int main(){
 
 	node* root = buildTree();
 
-	displayPreOrder(root);
-	cout<<endl;
+	// displayPreOrder(root);
+	// cout<<endl;
 
-	displayPostOrder(root);
-	cout<<endl;
+	// displayPostOrder(root);
+	// cout<<endl;
 
-	displayInOrder(root);
-	cout<<endl;
+	// displayInOrder(root);
+	// cout<<endl;
 
+	// displayLevelOrder(root);
+
+	// displayLevelOrderNewLine(root);
+
+	// cout<<numberOfNodes(root)<<endl;
+	// cout<<sumOfNodes(root)<<endl;
+
+	// cout<<height(root)<<endl;
+
+	// cout<<diameter(root)<<endl;
+
+	// if(isBalanced(root)){
+	// 	cout<<"Balanced"<<endl;
+	// }else{
+	// 	cout<<"Not Balanced"<<endl;
+	// }
+
+	// L2 pair = diameterOptimized(root);
+	// cout<<pair.diameter<<endl;
+	// cout<<pair.height<<endl;
+
+	int arr[]={}
 	return 0;
 }
 
 // 4 2 1 -1 -1 3 -1 -1 6 5 -1 -1 7 -1 -1
+
+// 18
+// 20
+// 22
+// 23
+// 25
+// 26
+// 27
+// 29
+// 30
+// 1
