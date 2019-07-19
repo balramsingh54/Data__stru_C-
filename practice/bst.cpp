81319629efcccb5810a378dec590292775493c5f
Balram <singhbalram054@gmail.com>
2019-07-19 21:11:56 +0530

    bst

---
+++ practice/bst.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include <queue>
+
+class node
+{
+	int data;
+	node* left;
+	node* right;
+
+	node(int data)
+	{
+		this->data=data;
+		left= NULL;
+		right= NULL;
+	}
+};
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
