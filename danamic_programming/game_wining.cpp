72edeb507f7a955ef380edddb72f5a19b3ff3b9b
Balram <singhbalram054@gmail.com>
2019-07-24 22:12:20 +0530

    dp advance

--- danamic_programming/game_wining.cpp
+++ danamic_programming/game_wining.cpp
@@ -6,6 +6,10 @@ int game_optimal(int arr, int i, int j)
 	int result;
 	int first=0;
 	int second=0;
+	
+	
+	result= max(first, second);
+	return result;
 }
 int main(int argc, char const *argv[])
 {
--- danamic_programming/mini_operation.cpp
+++ danamic_programming/mini_operation.cpp
@@ -22,6 +22,37 @@ int min_operation(string str1, string str2)
 
 // local  dp 
 
+int min_operation1(string str1, string str2)
+{
+	n= str1.length()+1;
+	m= str2.length()+1;
+	int dp[n][m];
+	int 
+	if (ch1==ch2)
+	{
+		min_operation(ros1, ros2);
+	}
+	for (int i = 0; i < str1.length(); i++)
+	{
+		for (int j = 0; i < s2.length(); j++)
+		{
+			if (i==0 or j==0)
+			{
+				dp[n][m]=i+j;
+				continue;
+			}
+			if (str1[i-1]==str2[j-1])
+			{
+				dp[i][j]=1+dp[i-1][j-1];
+			}
+			else 
+			{
+				dp[i][j]=max
+			}
+		}
+	}
+
+
 int min_operation_dp()
 
 int main(int argc, char const *argv[])
