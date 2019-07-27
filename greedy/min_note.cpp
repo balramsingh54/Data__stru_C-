90bb5ad91ab81c912201d4964aae3a9226544d7b
Balram <singhbalram054@gmail.com>
2019-07-27 12:39:54 +0530

    mini_note

--- greedy/min_note.cpp
+++ greedy/min_note.cpp
@@ -1,29 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int money_count(int arr[], int  money)
-{
-	int count=0;
-	while(money>0)
-	{
-		for (int i = 0; i < 10; i++)
-		{
-			if (money<arr[i-1])
-			{
-				money=money-arr[i-1];
-			}
-			if (i==10)
-			{
-				money=money-arr[i-1];
-				count++;
-			}
-		}
-	}
-}
 int main(int argc, char const *argv[])
 {
-	int arr[10]={1,2,5,10,20,50,100,200,500,2000};
-	int money=773;
+	int arr[10] = {1,2,5,10,20,50,100,200,500,2000};
+	int money;
+	cin>>money;
+
+	int count =0;
+
+	while(money>0){
+		int i=0;
+		for (i = 0; i < 10; ++i)
+		{
+			if(arr[i]>money){
+				money = money-arr[i-1];
+				cout<<arr[i-1]<<", ";
+				count++;
+				break;
+			}
+		}
+		if(i==10){
+			money = money-arr[i-1];
+			cout<<arr[i-1]<<", ";
+			count++;
+		}
+	}
+	cout<<endl;
+	cout<<count<<endl;
 
 	return 0;
 }
