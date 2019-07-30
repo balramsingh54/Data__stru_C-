56f7e64a17b5462652d1ed918e0e0ed5614b309e
Balram <singhbalram054@gmail.com>
2019-07-30 11:26:25 +0530

    heap

---
+++ heap/stlheap.cpp
@@ -0,0 +1,29 @@
+#include <iostream.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	//priority_queue<int> qu;// made automatic max heap 
+	// this is for min heap 
+	priority_queue<int, vector<int >, greater<int> >qu;
+	int h;
+	cin>>h;
+	while(eof)
+	{
+		if (!h=-1)
+		{
+			int k, l, m;
+			qu.push_back(k);
+			qu.push_back(l);
+			qu.push_back(m);
+			x=min(k,(min(l,m)))
+			if (/* condition */)
+			{
+				/* code */
+			}
+
+		}	}
+
+
+	return 0;
+}
